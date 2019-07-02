0fa2bc8a3301fc141afbc0414c206c983b98b786
Fer <ferlolgamer@hotmail.com>
2019-07-02 11:37:33 -0300

    Comienzo de Web API

---
+++ miwebapi/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace miwebapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> Get()
+        {
+            var alumnos = new List<Student>();
+            var A1 = new Student();
+            var A2 = new Student();
+            var A3 = new Student();
+            var A4 = new Student();
+            var A5 = new Student();
+            var A6 = new Student();
+
+            A1.SetPromedio(8);
+            A2.SetPromedio(6);
+            A3.SetPromedio(10);
+            A4.SetPromedio(3);
+            A5.SetPromedio(5);
+            A6.SetPromedio(1);
+            
+            alumnos.Add(A1);
+            alumnos.Add(A2);
+            alumnos.Add(A3);
+            alumnos.Add(A4);
+            alumnos.Add(A5);
+            alumnos.Add(A6);
+
+            return alumnos.OrderBy(p => p.Promedio).ToList();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ miwebapi/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace miwebapi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+    public class Person
+    {
+        public int edad { get; private set; }
+        public int Promedio { get; private set; }
+        public void Hello()
+        {
+
+            Console.WriteLine("hola");
+        }
+
+        public void SetAge(int n)
+        {
+            this.edad = n;
+
+        }
+        public void SetPromedio(int prom)
+        {
+            this.Promedio = prom;
+        }
+
+    }
+
+    public class Student : Person
+    {
+
+        public void showage()
+        {
+
+            Console.WriteLine("edad:" + this.edad);
+        }
+    }
+
+    public class Teacher : Person
+    {
+        public void explain()
+        {
+            Console.WriteLine("Explanation Begins");
+        }
+
+        private string subject { get; set; }
+    }
+
+
+
+}
+
+
